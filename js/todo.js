29ce89fb407c15f8cdd9ec45c1295805b9794ba3
Adi <aditya@eddywebs.com>
2015-05-13 15:39:18 -0400

    working release

--- js/todo.js
+++ js/todo.js
@@ -18,9 +18,23 @@ $(document).ready(function (){
     
     //pass all the task items to updateStatus plugin which has click handler to update task status
     $(".listItem").updateStatus();
-    
+    $(".deleteBtn").deleteTasks();
+
     console.log(tasks);
 });
+
+$.fn.deleteTasks=function(list){
+// dom element deletion -this jQuery function attaches a click handeler to all the elements passed which will delete the parent element on click
+// @param list - list of all the items to which the click handler is to be attached.
+    
+    return this.click(function(evt){
+        console.log(this);
+        var id =parseInt($(this).parent().attr("id"));
+        $(this).parent().remove() //delete the clicked element
+        deleteTask(id); //delete the task from the memory
+    });
+s
+};//end of deleteTasks()
 
 $.fn.updateStatus=function(list){
     
@@ -42,12 +56,11 @@ $.fn.updateStatus=function(list){
 
    // $(':checkbox').prop("checked", this.attributes["accomplished"].value);
 
-
     return this.click(function(evt){
-        console.log(this);
+       // console.log(this);
         //tasks[this.]
         var taskId= parseInt(this.attributes["id"].value);
-        console.log(tasks[taskId]);
+      //  console.log(tasks[taskId]);
         //set the clicked task as accomplished and save it to localStorage
         if(tasks[taskId].accomplished==false)
         {
@@ -79,16 +92,27 @@ function todo(todo) {
 
 function addTask(){
     task = document.getElementById("todo").value;
+    if(task ==null || task.trim() ==""){
+        alert("The task entered is blank, please enter a valid task.")
+        return;
+    }
     tasks.push(new todo(task));
     localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+function deleteTask(id){
+    tasks.splice(id, 1);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
 };
 function writeTaskList(tasks){    
     
     var output, count=0;
     output = "<table>";
     for(var task in tasks){
-        output += '<tr id="'+count+'" class="listItem" > <td> <input type="checkbox" chkbox /> </td>'; //add id attribute with value of array index for id
+        output += '<tr id="'+count+'" class="listItem" >';
+        output +='<td> <input type="checkbox" chkbox /> </td>'; //add id attribute with value of array index for id
         output += '<td class="taskText">'+tasks[task].task+'</td>';
+        output += '<td class="deleteBtn"><img src="img/recycle.png" width="22" height="22"></td>';
         //output += '<td>'+count+'</td>'
         output += '</tr>';
         count++;      
@@ -96,10 +120,10 @@ function writeTaskList(tasks){
     output += "</table>";
     return output
 };
-function checkTasks(tasks){
-//this method checks the status of each task(accomplished property)and marks checkbox against tasks accordingly
-    for(var task in tasks){
-        document.getElementById(task).checked = tasks[task].accomplished;
-    }
+// function checkTasks(tasks){
+// //this method checks the status of each task(accomplished property)and marks checkbox against tasks accordingly
+//     for(var task in tasks){
+//         document.getElementById(task).checked = tasks[task].accomplished;
+//     }
 
-};
+// };
